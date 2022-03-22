78bdd4e6f723a948805de7a3f5ba9c1b675d033a
Beto1821 <beto1821@uol.com.br>
2022-03-22 19:18:23 -0300

    ex09 rev00

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 let btn = document.querySelector('#criar-tarefa');
 let listaTarefas = document.getElementById('lista-tarefas');
 
+
 function addText() {
-  let itemLista = document.createElement('li');
-  let entrada = document.getElementById('texto-tarefa');
-  itemLista.innerText = entrada.value;
-  listaTarefas.appendChild(itemLista);
-  entrada.value = '';
+    let itemLista = document.createElement('li');
+    let entrada = document.getElementById('texto-tarefa');
+    itemLista.innerText = entrada.value;
+    listaTarefas.appendChild(itemLista);
+    itemLista.classList.add('li');
+    entrada.value = '';
 }
 btn.addEventListener('click', addText);
+
+let acendeLi = document.querySelector('#lista-tarefas')
+console.log(acendeLi);
+
+function alteraCor(event) {
+    const selecionada = document.querySelector('.selected');
+    if(selecionada) {
+        selecionada.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+    
+};
+acendeLi.addEventListener("click", alteraCor);
