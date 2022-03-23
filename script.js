b209f7bf39a9362dfda20e8e5aa4b972dceda0d4
Beto1821 <beto1821@uol.com.br>
2022-03-22 21:31:29 -0300

    ex10 rev00

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 let btn = document.querySelector('#criar-tarefa');
 let listaTarefas = document.getElementById('lista-tarefas');
 
-
 function addText() {
-    let itemLista = document.createElement('li');
-    let entrada = document.getElementById('texto-tarefa');
-    itemLista.innerText = entrada.value;
-    listaTarefas.appendChild(itemLista);
-    itemLista.classList.add('li');
-    entrada.value = '';
+  let itemLista = document.createElement('li');
+  let entrada = document.getElementById('texto-tarefa');
+  itemLista.innerText = entrada.value;
+  listaTarefas.appendChild(itemLista);
+  itemLista.classList.add('li');
+  entrada.value = '';
 }
 btn.addEventListener('click', addText);
 
-let acendeLi = document.querySelector('#lista-tarefas')
-console.log(acendeLi);
+let acendeLi = document.querySelector('#lista-tarefas');
 
 function alteraCor(event) {
-    const selecionada = document.querySelector('.selected');
-    if(selecionada) {
-        selecionada.classList.add('completed')
-        selecionada.classList.remove('selected');
-    }
-    event.target.classList.add('selected');
+  const selecionada = document.querySelector('.selected');
+  if (selecionada) {
+    selecionada.classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+acendeLi.addEventListener('click', alteraCor);
+
+function tiraRisco(event) {
+  event.target.classList.toggle('completed');
+ 
+}
+
+acendeLi.addEventListener('dblclick', tiraRisco);
+
+let btn2 = document.querySelector("#apaga-tudo");
+btn2.addEventListener("click", function() {
     
-    
-};
-acendeLi.addEventListener("click", alteraCor);
+    location.reload();
+});
